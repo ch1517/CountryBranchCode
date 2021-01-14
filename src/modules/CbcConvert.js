99cba88099c56090b494ddd2b2334d924cc0f8ca
Suhyeon Jeong <25702775+ch1517@users.noreply.github.com>
2021-01-14 18:47:27 +0900

    100*100, header 추가

--- src/modules/CbcConvert.js
+++ src/modules/CbcConvert.js
@@ -44,7 +44,6 @@ function HangleLineArray() {
                 } else {
                     cbc = "dd"
                 }
-                console.log("dddddd", cbc);
                 reArr.push({
                     latLongArr: [[c[1], c[0]], [nx[1], nx[0]], [nxy[1], nxy[0]], [ny[1], ny[0]]],
                     id: i.toString() + j.toString() + "x",
@@ -55,47 +54,73 @@ function HangleLineArray() {
     }
     return reArr;
 }
-function HangleLineArray2(start, end) {
+function smallPointXY(m, minX, maxX, minY, maxY) {
+    var p;
+    var TKM = 100000;
+    console.log(minX, maxX, minY, maxY);
+    // 현재 bound가 700000~1500000, 1300000~2200000 이내인가
+    // 이내라면 bound 그대로 이용, 아니라면 최대 최소 지정
+    minX = (minX > 7 * TKM ? minX : 7 * TKM);
+    maxX = (maxX > 15 * TKM ? 15 * TKM : maxX);
+    minY = (minY > 13 * TKM ? minY : 13 * TKM);
+    maxY = (maxY > 22 * TKM ? 22 * TKM : maxY);
+
+    console.log(minX, maxX, minY, maxY);
+
+    minX = (parseInt(minX / m) * m < minX ? (parseInt(minX / m) - 1) * m : parseInt(minX / m) * m);
+    minY = (parseInt(minY / m) * m < minY ? (parseInt(minY / m) - 1) * m : parseInt(minY / m) * m);
+    maxX = (parseInt(maxX / m) * m < maxX ? (parseInt(maxX / m) + 1) * m : parseInt(maxX / m) * m);
+    maxY = (parseInt(maxY / m) * m < maxY ? (parseInt(maxY / m) + 1) * m : parseInt(maxY / m) * m);
+
+    console.log(parseInt(maxX / m) * m, maxX);
     var pArr = [];
+    for (var x = minX; x <= maxX; x += m) {
+        var t = [];
+        for (var y = minY; y <= maxY; y += m) {
+            p = proj4(grs80, wgs84, [x, y]);
+            t.push(p);
+        }
+        pArr.push(t);
+    }
+    return pArr;
+}
+function smallLineArray(zoomLevel, start, end) {
     var reArr = [];
     var start = proj4(wgs84, grs80, [start.lng, start.lat]);
     var end = proj4(wgs84, grs80, [end.lng, end.lat]);
-    console.log(start);
-    console.log(end);
-    // for (var x = 7 * TKM; x < 15 * TKM; x += m) {
-    //     var t = [];
-    //     for (var y = 13 * TKM; y < 22 * TKM; y += m) {
-    //         p = proj4(grs80, wgs84, [x, y]);
-    //         t.push(p);
-    //     }
-    //     pArr.push(t);
-    // }
-    // var filter = [[0, 9], [0, 7], [1, 9], [2, 9], [2, 9], [4, 9], [5, 9], [6, 9]];
-    // for (var i = 0; i < pArr.length; i++) {
-    //     for (var j = filter[i][0]; j < filter[i][1]; j++) {
-    //         if (i < pArr.length - 1 && j < pArr[i].length - 1) {
-    //             var c = pArr[i][j];
-    //             var nx = pArr[i + 1][j];
-    //             var ny = pArr[i][j + 1];
-    //             var nxy = pArr[i + 1][j + 1];
-    //             var cbc = converter([(nxy[0] + c[0]) / 2, (nxy[1] + c[1]) / 2])
-    //             if (cbc != undefined) {
-    //                 cbc = cbc[0]
-    //             } else {
-    //                 cbc = "dd"
-    //             }
-    //             console.log("dddddd", cbc);
-    //             reArr.push({
-    //                 latLongArr: [[c[1], c[0]], [nx[1], nx[0]], [nxy[1], nxy[0]], [ny[1], ny[0]]],
-    //                 id: i.toString() + j.toString() + "x",
-    //                 cbcText: cbc
-    //             });
-    //         }
-    //     }
-    // }
+    if (zoomLevel > 10) {
+        var pArr = smallPointXY(10000, start[0], end[0], start[1], end[1]);
+    } else {
+        var pArr = []
+
+    }
+
+    for (var i = 0; i < pArr.length; i++) {
+        for (var j = 0; j < pArr[0].length; j++) {
+            if (i < pArr.length - 1 && j < pArr[i].length - 1) {
+                var c = pArr[i][j];
+                var nx = pArr[i + 1][j];
+                var ny = pArr[i][j + 1];
+                var nxy = pArr[i + 1][j + 1];
+                var cbc = converter([(nxy[0] + c[0]) / 2, (nxy[1] + c[1]) / 2])
+                if (cbc != undefined) {
+                    cbc = cbc[0]
+                } else {
+                    cbc = "dd"
+                }
+                reArr.push({
+                    latLongArr: [[c[1], c[0]], [nx[1], nx[0]], [nxy[1], nxy[0]], [ny[1], ny[0]]],
+                    id: i.toString() + j.toString() + "y",
+                    cbcText: cbc
+                });
+            }
+        }
+    }
+
+    console.log(reArr);
     return reArr;
 }
 function lineArray() {
     return HangleLineArray()
 }
-export default { converter, lineArray, HangleLineArray2 };
+export default { converter, lineArray, smallLineArray };
--- src/modules/Maps.js
+++ src/modules/Maps.js
@@ -3,50 +3,72 @@ import { MapContainer, TileLayer, Polygon, Marker, Popup, Tooltip, useMap, useMa
 import CbcConvert from './CbcConvert';
 import { useState } from "react";
 function ZoomLevelCheck() {
-    const [zoomLevel, setZoomLevel] = useState(5); // initial zoom level provided for MapContainer
+    const [zoomLevel, setZoomLevel] = useState(7); // initial zoom level provided for MapContainer
+    const [lineArr, setLineArr] = useState([]);
     const map = useMap();
     const mapEvents = useMapEvents({
         zoomend: () => {
             setZoomLevel(mapEvents.getZoom());
+            setLineArr(CbcConvert.smallLineArray(zoomLevel, map.getBounds()._southWest, map.getBounds()._northEast));
         },
         moveend: () => {
-            console.log();
-            console.log(map.getBounds()._southWest);
-            CbcConvert.HangleLineArray2(map.getBounds()._northEast, map.getBounds()._southWest);
-
+            setLineArr(CbcConvert.smallLineArray(zoomLevel, map.getBounds()._southWest, map.getBounds()._northEast));
         }
     });
     if (zoomLevel > 15) {
 
     }
-    console.log(zoomLevel);
+    if (lineArr.length != 0) {
+        return (
+            <div>
+                {lineArr.map(({ id, latLongArr, cbcText }) => {
+                    return <Polygon key={id} positions={latLongArr} color={'white'}
+                        eventHandlers={{
+                            click: (e) => {
+                                console.log([e.latlng["lat"], e.latlng["lng"]]);
+                                CbcConvert.converter([e.latlng["lng"], e.latlng["lat"]]);
+                            },
+                        }}>
+                        <Tooltip direction='bottom' opacity={1} permanent>
+                            <span>{cbcText}</span>
+                        </Tooltip>
+                    </Polygon>
 
-    return null
+                })}
+            </div>
+        )
+    } else {
+        return null;
+    }
+
+
+
 }
 class Maps extends Component {
     constructor() {
         super();
         this.state = {
-            lat: 36.35046532102072,
-            lng: 127.38477578320756,
-            zoom: 16,
+            lat: 36.09698006901975,
+            lng: 129.38089519358994,
+            zoom: 7,
             data: CbcConvert.lineArray(),
         }
     }
+
     render() {
         const position = [this.state.lat, this.state.lng];
         CbcConvert.converter([126.98823732740473, 37.55122041521281]);
-        // console.log(CbcConvert.lineArray());
 
         return (
             <div>
-                <MapContainer style={{ height: "100vh" }} center={position} zoom={this.state.zoom}>
-                    <ZoomLevelCheck />
+                <MapContainer style={{ height: "100vh" }} center={position} zoom={this.state.zoom} scrollWheelZoom={true}>
+
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url='https://api.vworld.kr/req/wmts/1.0.0/5FE9AB0A-3B34-32F3-A646-1133D92EF014/midnight/{z}/{y}/{x}.png'
 
                     />
+                    <ZoomLevelCheck />
                     {this.state.data.map(({ id, latLongArr, cbcText }) => {
                         return <Polygon key={id} positions={latLongArr} color={'white'}
                             eventHandlers={{
@@ -59,6 +81,7 @@ class Maps extends Component {
                                 <span>{cbcText}</span>
                             </Tooltip>
                         </Polygon>
+
                     })}
                     <Marker position={position}>
                         <Popup>
