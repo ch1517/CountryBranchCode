6fd96ca185bb1686495ed335b266180ebe4ace3d
Suhyeon Jeong <doqtqu@gmail.com>
2024-08-17 15:30:57 +0900

    Chore: lint 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+		react(), 
+		checker({
+			typescript: true,
+			eslint: {
+				lintCommand: 'eslint',
+				dev: {
+					logLevel: ['error', 'warning'],
+				},
+			},
+		}),
+	],
   define: {
     'process.env': {}
   },
+	resolve: {
+		alias: [
+			{ find: "~", replacement: path.resolve(__dirname, "src") },
+		],
+	},
 });
